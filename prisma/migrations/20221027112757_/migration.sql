face411f6bbc151b6712d7d8117ce74d5cb95d12
Daniil <dan.bardyuk@yandex.ru>
2022-10-28 10:19:38 +0300

    refactore migration

--- prisma/migrations/20221027112757_/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "TodoItem" (
-    "id" SERIAL NOT NULL,
-    "text" TEXT NOT NULL,
-    "state" BOOLEAN NOT NULL,
-
-    CONSTRAINT "TodoItem_pkey" PRIMARY KEY ("id")
-);
